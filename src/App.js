495664189c638fc25571afb08d0f735948953ba7
mandyrush <mandyrush85@gmail.com>
2021-11-01 19:33:34 -0600

    Show beer info, create like/unlike functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Reset.css';
 import './App.css';
 
 import Beers from './components/Beers';
@@ -10,12 +11,30 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      beers: []
+      beers: [],
+      likedBeers: []
     }
   }
 
   componentDidUpdate() {
     console.log('This is from state ', this.state.beers);
+  }
+
+  handleLike = (id) => {
+    console.log('Clicked - Id: ', id);
+    // Check if the current beer is liked, if it is, remove it from the list
+    // If it isn't add it to the list
+    if (this.state.likedBeers.includes(id)) {
+      let updatedLikes = this.state.likedBeers.filter(beerId => beerId !== id);
+      console.log('Updated Likes: ', updatedLikes);
+      this.setState({
+        likedBeers: updatedLikes
+      })
+    } else {
+      this.setState({
+        likedBeers: [...this.state.likedBeers, id]
+      });
+    }
   }
 
   // Promises
@@ -29,6 +48,7 @@ class App extends React.Component {
 
   // Async / Await
   async componentDidMount() {
+    // Get beer info and store it
     let response = await fetch(PUNK_API);
     let beers = await response.json();
     this.setState({
@@ -40,7 +60,10 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>Punk API</h1>
-        <Beers beers={this.state.beers} />
+        <Beers 
+          beers={this.state.beers} 
+          likedBeers={this.state.likedBeers} 
+          handleLike={this.handleLike} />
       </div>
     );
   }
--- src/components/Beers.js
+++ src/components/Beers.js
@@ -1,13 +1,72 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faHeartOutline } from '@fortawesome/free-regular-svg-icons';
+
+const beerDetails = {
+    listStyle: 'none',
+    display: 'grid',
+    gridTemplateColumns: '20% 80%',
+    paddingBottom: '3rem'
+}
+
+const headerStyles = {
+    display: 'flex'
+}
+
+const likeBtn = {
+    background: 'transparent',
+    border: 'none',
+    cursor: 'pointer'
+}
+
+const likeBtnIconSolid = {
+    color: 'orange',
+}
+
+const likeBtnIconOutline = {
+    color: 'orange'
+}
+
 function Beers(props) {
     return (
         <ul>
             {props.beers.map(beer => (
-                <li key={beer.id}>
+                <li style={beerDetails} key={beer.id}>
                     <img src={beer.image_url} alt={beer.name} />
-                    <p>{beer.name}</p>
-                    <button>Like Btn</button>
+                    <article>
+                        <header style={headerStyles}>
+                            <h2>{beer.name}</h2>
+                            {/* If the beer is liked, show a filld in heart icon
+                            If the beer is not liked show a heart outline icon */}
+                            <button style={likeBtn} onClick={() => {props.handleLike(beer.id)}}>
+                                <FontAwesomeIcon 
+                                    icon={props.likedBeers.includes(beer.id) ? faHeart : faHeartOutline} 
+                                    style={props.likedBeers.includes(beer.id) ? likeBtnIconSolid : likeBtnIconOutline} />
+                            </button>
+                        </header>
+                        <p><em>{beer.tagline}</em></p>
+                        <p><strong>ABV:</strong> {beer.abv}</p>
+                        <p><strong>IBU:</strong> {beer.ibu}</p>
+                        <p><strong>Description:</strong> {beer.description}</p>
+                        <p><strong>Malts:</strong></p>
+                        <ul>
+                            {beer.ingredients.malt.map((ingredient, index) => (
+                                <li key={index}>
+                                    {ingredient.name} - {ingredient.amount.value} {ingredient.amount.unit}
+                                </li>
+                            ))}
+                        </ul>
+                        <p><strong>Food Pairing:</strong></p>
+                        <ul>
+                            {beer.food_pairing.map((pairing, index) => (
+                                <li key={index}>
+                                    {pairing}
+                                </li>
+                            ))}
+                        </ul>
+                    </article>
                 </li>
             ))}
         </ul>
